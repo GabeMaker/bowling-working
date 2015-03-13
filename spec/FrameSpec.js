1df64ecd8ae1fa8dab8c690b00a453e5927e492e
gabe <GabeMaker@users.noreply.github.com>
2015-03-13 14:51:00 +0000

    can record roll 1

--- spec/FrameSpec.js
+++ spec/FrameSpec.js
@@ -2,10 +2,16 @@ describe ("Frame", function() {
 
   var frame;
 
-  describe ("rolling", function() {
+  describe ("scoring", function() {
     it("has no score to start", function() {
       frame = new Frame;
       expect(frame.score).toBe(0);
+    });
+
+    it("can record first roll", function() {
+      frame = new Frame;
+      frame.rollOne(5);
+      expect(frame.rollOne).toBe(5);
     });
 
   });
--- src/Frame.js
+++ src/Frame.js
@@ -1,3 +1,12 @@
 var Frame = function() {
   this.score = 0;
 };
+
+Frame.prototype.rollOne = function(roll) {
+  this.rollOne = roll;
+};
+
+// Frame.prototype.score = function() {
+//   this.score = this.rollOne + this.rollTwo;
+//   return this.score
+// };
